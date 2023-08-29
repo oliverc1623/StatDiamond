18cd0c3e5164e89a205b65813889e90677f91370
Oliver Chang <oliverc1622@gmail.com>
2023-08-29 12:29:52 -0700

    heat map now a function

--- find_batting_strategy.py
+++ find_batting_strategy.py
@@ -4,32 +4,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-pitch_count_labels = [
-    "(0,0)",
-    "(1,0)",
-    "(2,0)",
-    "(3,0)",
-    "(0,1)",
-    "(0,2)",
-    "(1,1)",
-    "(1,2)",
-    "(2,1)",
-    "(2,2)",
-    "(3,1)",
-    "(3,2)",
-    "Out", 
-    "Single",
-    "Double",
-    "Triple",
-    "HR",
-    "Walk"
-]
-
-def main(args):
-    pitcher_mdp = PitcherMDP(args.first_name, args.last_name, args.start_dt, args.end_dt)
-    M = pitcher_mdp.get_markov_chain(no_actions=True)
-    print(M)
-
+def plot_markov_chain(M, first_name, last_name, year):
+    pitch_count_labels = ["(0,0)","(1,0)","(2,0)","(3,0)","(0,1)","(0,2)","(1,1)","(1,2)","(2,1)","(2,2)","(3,1)","(3,2)","Out", "Single","Double","Triple","HR","Walk"]
     mask = np.tril(np.ones_like(M))
     h = sns.heatmap(M, 
                 cmap='viridis', 
@@ -38,9 +14,14 @@ def main(args):
                 mask=mask,
                 annot=True,
                 xticklabels=pitch_count_labels,
-                yticklabels=pitch_count_labels).set_title(f"Pitch Count Sequence Transition Probability for {args.first_name} {args.last_name} in {args.start_dt[:4]}")
+                yticklabels=pitch_count_labels).set_title(f"Pitch Count Sequence Transition Probability for {first_name} {last_name} in {year}")
     plt.show()
 
+def main(args):
+    pitcher_mdp = PitcherMDP(args.first_name, args.last_name, args.start_dt, args.end_dt)
+    M = pitcher_mdp.get_markov_chain(no_actions=True)
+    print(M)
+    plot_markov_chain(M, args.first_name, args.last_name, args.start_dt[:4])
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
