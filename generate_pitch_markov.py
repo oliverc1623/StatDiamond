018ded782cef8a32c29c3e155e67decf2a30f729
Oliver Chang <oliverc1622@gmail.com>
2023-08-28 22:54:34 -0700

    updated pitch_markov by including terminal states

--- generate_pitch_markov.py
+++ generate_pitch_markov.py
@@ -30,14 +30,79 @@ states = {
     "HR": 16, 
     "Walk": 17
 }
+pitch_count_labels = [
+    "(0,0)",
+    "(1,0)",
+    "(2,0)",
+    "(3,0)",
+    "(0,1)",
+    "(0,2)",
+    "(1,1)",
+    "(1,2)",
+    "(2,1)",
+    "(2,2)",
+    "(3,1)",
+    "(3,2)",
+    "Out", 
+    "Single",
+    "Double",
+    "Triple",
+    "HR",
+    "Walk"
+]
 
 def get_pitch_sequence(df_game):
     pitch_seq = []
+    action_seq = []
+    walk_flag = False
     for i, row in df_game.iterrows():
         event = row['events']
-        if str(event) == "nan":
+
+        # no outcome, add non-terminal state
+        if str(event) == "nan" or str(event) == "field_error" or str(event) == "fielders_choice":
             pitch_seq.append(states[row['non_terminal_states']])
-    return pitch_seq
+        # if Out, add terminal state
+        elif (str(event) == "strikeout" or 
+                str(event) == "field_out" or 
+                str(event) == "grounded_into_double_play" or 
+                str(event) == "fielders_choice_out" or 
+                str(event) == "sac_fly" or 
+                str(event) == "force_out" or 
+                str(event) == "sac_bunt" or 
+                str(event) == "caught_stealing_2b" or 
+                str(event) == "double_play"):
+            pitch_seq.append(states["Out"])
+        elif (str(event) == "single"):
+            pitch_seq.append(states["Single"])
+        elif (str(event) == "double"):
+            pitch_seq.append(states["Double"])
+        elif (str(event) == "triple"):
+            pitch_seq.append(states["Triple"])
+        elif (str(event) == "home_run"):
+            pitch_seq.append(states["HR"])
+        elif (str(event) == "walk"):
+            pitch_seq.append(states[row['non_terminal_states']])
+            pitch_seq.append(states["Walk"])
+            walk_flag = True
+
+        # if swing
+        if (row['description'] == 'foul' or 
+             row['description'] == 'swinging_strike' or 
+             row['description'] == 'foul_tip' or
+             row['description'] == 'swinging_strike_blocked' or
+             row['description'] == 'foul_bunt' or 
+             row['description'] == 'missed_bunt' or 
+             row['description'] == 'hit_into_play'):
+            action_seq.append("swing")
+        # elif stand
+        elif (row['description'] == 'called_strike' or 
+              row['description'] == 'ball' or 
+              row['description'] == 'blocked_ball'):
+            action_seq.append("stand")
+            if walk_flag:
+                action_seq.append("stand")
+                walk_flag = False
+    return pitch_seq, action_seq
 
 def count_transitions(pitch_seq):
     n = 1+ max(pitch_seq) #number of states
@@ -71,40 +136,32 @@ def main(args):
     df_simple['non_terminal_states'] = non_terminal_states
     df_simple = df_simple.reset_index()
 
-    # Make a dictionary for each game
-    unique_games = df_simple.game_date.unique()
-    DataFrameDict = {elem : pd.DataFrame() for elem in unique_games}
+    pitch_seq, action_seq = get_pitch_sequence(df_simple) # TODO: utilized action_seq
+    # Sample DataFrame with a 'state' column
+    data = {'state': pitch_seq}
+    df = pd.DataFrame(data)
 
-    for key in DataFrameDict.keys():
-        DataFrameDict[key] = df_simple[:][df_simple.game_date == key]
+    # Create a new column 'next_state' that represents the next state for each row
+    df['next_state'] = df['state'].shift(-1)
 
-    all_transition_matrices = []
-    for game_date, game_df in DataFrameDict.items():
-        pitch_sequence = get_pitch_sequence(game_df)
-        m = count_transitions(pitch_sequence)
-        pad_len = 12 - len(m)
-        m = np.pad(m, (0, pad_len), mode="constant")
-        all_transition_matrices.append(m)
-    all_transition_matrices = np.array(all_transition_matrices)
-    M = sum(all_transition_matrices)
-    M = get_probabilities(M.tolist())
-    # for row in M: print(' '.join('{0:.2f}'.format(x) for x in row))
-    M = np.triu(np.array(M))
+    # Remove the last row since it doesn't have a next state
+    df = df[:-1]
 
-    M = pd.DataFrame(M)
-    M = M.rename(columns={0:"(0,0)",
-                    1:"(1,0)",
-                    2:"(2,0)",
-                    3:"(3,0)",
-                    4:"(0,1)",
-                    5:"(0,2)",
-                    6:"(1,1)",
-                    7:"(1,2)",
-                    8:"(2,1)",
-                    9:"(2,2)",
-                    10:"(3,1)",
-                    11:"(3,2)"})
-    h = sns.heatmap(M, xticklabels=M.columns, yticklabels=M.columns)
+    # Calculate transition counts
+    transition_counts = df.groupby(['state', 'next_state']).size().unstack(fill_value=0)
+
+    # Calculate transition probabilities
+    transition_probabilities = transition_counts.div(transition_counts.sum(axis=1), axis=0)
+
+    mask = np.tril(np.ones_like(transition_probabilities))
+    h = sns.heatmap(transition_probabilities, 
+                cmap='viridis', 
+                cbar=True,
+                fmt='.2f',
+                mask=mask,
+                xticklabels=pitch_count_labels,
+                yticklabels=pitch_count_labels)
+    plt.show()
     if args.save:
         fig = h.get_figure()
         fig.savefig(f'figures/{args.pitcher_file[5:-4]}_markov_matrix.pdf')
