5eda5156ac8f2cc0e88c7966a3999560ad3cd55f
Oliver Chang <oliverc1622@gmail.com>
2023-08-31 23:20:52 -0700

    wrote function for one iter

--- ValueIteration.py
+++ ValueIteration.py
@@ -3,11 +3,11 @@ import numpy as np
 class ValueIteration:
     def __init__(self, reward_function, transition_model, gamma) -> None:
         self.num_states = transition_model.shape[0]
-        self.num_actions = transition_model.shape[1]
+        self.num_actions = transition_model.shape[2]
         self.reward_function = np.nan_to_num(reward_function)
         self.transition_model = transition_model
         self.gamma = gamma
-        self.values = np.zeros(self.num_states)
+        self.values = np.zeros(self.transition_model.shape[1])
         self.policy = None
 
     def get_policy(self, q_table):
@@ -17,27 +17,30 @@ class ValueIteration:
             for a in ['stand', 'swing']:
                 pass
 
-    def value_iteration(self, P):
-        q_table = np.zeros((18))
+    def one_iteration(self):
+        delta = 0
+        for s in range(self.num_states):
+            temp = self.values[s]
+            v_list = np.zeros(self.num_actions)
+            for a in range(self.num_actions):
+                new_value = 0.0
+                for j in range(s, self.transition_model.shape[1]):
+                    p = self.transition_model[s,j,a]
+                    reward = self.reward_function(s, a, j)
+                    new_value += p*(reward + self.values[j])
+                v_list[a] = new_value
+            self.values[s] = max(v_list)
+            delta = max(delta, abs(temp - self.values[s]))
+        return delta
+
+    def train(self):
+        epoch = 0
         delta = np.Inf
         epsilon = np.finfo(float).eps
         while delta >= epsilon:
-            for i in range(0, 12): # TODO: replace range with len of var
-                v = q_table[i]
-                swing_value = 0
-                stand_value = 0
-                for j in range(i, 18): # TODO: replace range with len of var
-                    p_stand = P[(P['state']==i) & (P['next_state']==j)].stand
-                    p_swing = P[(P['state']==i) & (P['next_state']==j)].swing
-                    if not p_stand.empty:
-                        reward = self.reward_fn(i,"stand",j)
-                        stand_value += p_stand.item()*(reward + q_table[j])
-                    if not p_swing.empty:
-                        reward = self.reward_fn(i,"swing",j)
-                        swing_value += p_swing.item()*(reward + q_table[j])
-                q_table[i] = max(swing_value, stand_value)
-                delta = min(delta, np.abs(v - q_table[i]))
-        print("Converged!")
-        print(q_table)
-        policy = self.get_policy(q_table)
-        return policy
+            epoch += 1
+            delta = self.one_iteration()
+        print(f"Converged in {epoch} epochs!")
+        print(self.values)
+        # policy = self.get_policy(q_table)
+        return None
